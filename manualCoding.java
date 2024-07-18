13e643dd0d927f6981271fe5e7327dafc692843f
Stargaz <119311723+Stargazzy@users.noreply.github.com>
2024-07-17 19:39:14 -0500

    removed if statement

--- manualCoding.java
+++ manualCoding.java
@@ -1,9 +1,7 @@
 public class manualCoding{
 public static void main (String [] args){
 System.out.println("Hello!");
-if (5> 0){
 System.out.println("How are you?");
-  System.out.println("Practicing fetch");
-    }
+  System.out.println("Practicing fetch");  
   }
 }
