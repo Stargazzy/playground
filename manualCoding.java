ef308614fdcb6e6dfa2cdac1dfc8a7b9bb1e533b
Stargaz <119311723+Stargazzy@users.noreply.github.com>
2024-07-17 19:49:20 -0500

    Added one more line of code

--- manualCoding.java
+++ manualCoding.java
@@ -3,5 +3,6 @@ public static void main (String [] args){
 System.out.println("Hello!");
 System.out.println("How are you?");
   System.out.println("Practicing fetch");  
+  System.out.println("Salam!");
   }
 }
