e517fcfa41952faf791ffe160ad4bc5960a82a40
Nur Berdibekov <nurberdibekov@Nurs-MBP.hsd1.il.comcast.net>
2024-07-17 20:06:04 -0500

    Revert "Added one more line of code"
    This reverts commit ef308614fdcb6e6dfa2cdac1dfc8a7b9bb1e533b.

--- manualCoding.java
+++ manualCoding.java
@@ -5,10 +5,13 @@ if (5> 0){
 System.out.println("How are you?");
 <<<<<<< HEAD
   System.out.println("Practicing fetch");  
+<<<<<<< HEAD
   System.out.println("Salam!");
 =======
   System.out.println("Practicing fetch");
     }
 >>>>>>> parent of 13e643d (removed if statement)
+=======
+>>>>>>> parent of ef30861 (Added one more line of code)
   }
 }
