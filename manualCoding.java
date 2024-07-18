bc05cfe9ff53d9bb4452a0a421c82aaa9c9ec02a
Nur Berdibekov <nurberdibekov@Nurs-MBP.hsd1.il.comcast.net>
2024-07-17 20:03:59 -0500

    reverted to the previous one
    This reverts commit 13e643dd0d927f6981271fe5e7327dafc692843f.

--- manualCoding.java
+++ manualCoding.java
@@ -1,8 +1,14 @@
 public class manualCoding{
 public static void main (String [] args){
 System.out.println("Hello!");
+if (5> 0){
 System.out.println("How are you?");
+<<<<<<< HEAD
   System.out.println("Practicing fetch");  
   System.out.println("Salam!");
+=======
+  System.out.println("Practicing fetch");
+    }
+>>>>>>> parent of 13e643d (removed if statement)
   }
 }
